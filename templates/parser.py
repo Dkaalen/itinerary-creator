cc9ce034d7c677ca8c0f1d2515dcee15357ee962
Dkaalen <denniskaalen@gmail.com>
2026-05-20 15:49:13 +0200

    Remove accidental templates folder

--- templates/parser.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility shim. The parser module was renamed to itinerary_parser.py.
-
-New code should import from itinerary_parser instead of parser.
-"""
-
-from itinerary_parser import *  # noqa: F401,F403
